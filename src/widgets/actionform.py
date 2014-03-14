40a05ffea92348dda84b8c56894f17edd9f92092
sblanch <sblanch@a18efa20-c439-0410-bf20-9b91ec695b4c>
2014-03-14 14:44:11 +0000

    new widget to contain the set of widgets for the switch actions. From the labview-like button this is more a taurus version of it.
    git-svn-id: svn+ssh://controls01/siciliarep/svn/trunk/tango_client/ctlinac/trunk@12424 a18efa20-c439-0410-bf20-9b91ec695b4c

---
+++ src/widgets/actionform.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# Code implementation generated from reading ui file 'ui/actionWidget.ui'
+#
+# Created: Fri Mar 14 13:21:51 2014 
+#      by: Taurus UI code generator 3.1.1
+#
+# WARNING! All changes made in this file will be lost!
+
+__docformat__ = 'restructuredtext'
+
+import sys
+import PyQt4.Qt as Qt
+from ui_actionWidget import Ui_actionForm
+from taurus.qt.qtgui.panel import TaurusWidget
+
+class actionForm(TaurusWidget):
+
+    def __init__(self, parent=None, designMode=False):
+        TaurusWidget.__init__(self, parent, designMode=designMode)
+        
+        self._ui = Ui_actionForm()
+        self._ui.setupUi(self)
+        
+    
+    @classmethod
+    def getQtDesignerPluginInfo(cls):
+        ret = TaurusWidget.getQtDesignerPluginInfo()
+        ret['module'] = 'actionform'
+        ret['group'] = 'Taurus Linac'
+        ret['container'] = ':/designer/dialogbuttonbox.png'
+        ret['container'] = False
+        return ret
+
+
+def main():
+    app = Qt.QApplication(sys.argv)
+    w = actionForm()
+    w.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
---
+++ src/widgets/ui_actionWidget.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'widgets/ui/actionWidget.ui'
+#
+# Created: Fri Mar 14 15:29:38 2014
+#      by: PyQt4 UI code generator 4.9.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+class Ui_actionForm(object):
+    def setupUi(self, actionForm):
+        actionForm.setObjectName(_fromUtf8("actionForm"))
+        actionForm.resize(86, 41)
+        self.gridLayout_2 = QtGui.QGridLayout(actionForm)
+        self.gridLayout_2.setMargin(0)
+        self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
+        self.actionFrame = QtGui.QFrame(actionForm)
+        self.actionFrame.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.actionFrame.setFrameShadow(QtGui.QFrame.Raised)
+        self.actionFrame.setObjectName(_fromUtf8("actionFrame"))
+        self.gridLayout = QtGui.QGridLayout(self.actionFrame)
+        self.gridLayout.setMargin(0)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.Check = LinacValueCheckBox(self.actionFrame)
+        self.Check.setLayoutDirection(QtCore.Qt.RightToLeft)
+        self.Check.setShowText(False)
+        self.Check.setObjectName(_fromUtf8("Check"))
+        self.gridLayout.addWidget(self.Check, 0, 2, 1, 1)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem, 0, 0, 1, 1)
+        self.Led = TaurusLed(self.actionFrame)
+        self.Led.setMinimumSize(QtCore.QSize(15, 15))
+        self.Led.setMaximumSize(QtCore.QSize(15, 15))
+        self.Led.setObjectName(_fromUtf8("Led"))
+        self.gridLayout.addWidget(self.Led, 0, 1, 1, 1)
+        spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.gridLayout.addItem(spacerItem1, 0, 3, 1, 1)
+        self.Label = QtGui.QLabel(self.actionFrame)
+        font = QtGui.QFont()
+        font.setPointSize(7)
+        self.Label.setFont(font)
+        self.Label.setAlignment(QtCore.Qt.AlignCenter)
+        self.Label.setObjectName(_fromUtf8("Label"))
+        self.gridLayout.addWidget(self.Label, 1, 0, 1, 4)
+        self.gridLayout_2.addWidget(self.actionFrame, 0, 0, 1, 1)
+
+        self.retranslateUi(actionForm)
+        QtCore.QMetaObject.connectSlotsByName(actionForm)
+
+    def retranslateUi(self, actionForm):
+        actionForm.setWindowTitle(QtGui.QApplication.translate("actionForm", "Form", None, QtGui.QApplication.UnicodeUTF8))
+        self.Label.setText(QtGui.QApplication.translate("actionForm", "true/false", None, QtGui.QApplication.UnicodeUTF8))
+
+from taurus.qt.qtgui.display import TaurusLed
+from taurus.qt.qtgui.panel import TaurusWidget
+from linacvaluecheckbox import LinacValueCheckBox
