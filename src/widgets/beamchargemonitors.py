41db150f714065b131d5a734c8ff074dd97a3ecf
Sergi Blanch-Torné <sergi@calcurco.cat>
2017-02-21 11:08:20 +0100

    migrate beamchargemonitors to UILoadable

--- src/widgets/beamchargemonitors.py
+++ src/widgets/beamchargemonitors.py
@@ -1,27 +1,52 @@
-#!/usr/bin/env python
-
-# Code implementation generated from reading ui file 'ui/beamChargeMonitors.ui'
+# ##### BEGIN GPL LICENSE BLOCK #####
 #
-# Created: Thu Mar 13 12:45:27 2014 
-#      by: Taurus UI code generator 3.1.1
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 3
+#  of the License, or (at your option) any later version.
 #
-# WARNING! All changes made in this file will be lost!
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
 
-__docformat__ = 'restructuredtext'
+__author__ = "Sergi Blanch-Torne"
+__copyright__ = "Copyright 2015, CELLS / ALBA Synchrotron"
+__license__ = "GPLv3+"
 
-import sys
+import os
 from taurus.qt import Qt
-from ui_beamChargeMonitors import Ui_beamChargeMonitors
 from taurus.qt.qtgui.panel import TaurusWidget
+from taurus.qt.qtgui.util.ui import UILoadable
+import traceback
 
+@UILoadable(with_ui="_ui")
 class beamChargeMonitors(TaurusWidget):
 
-    def __init__(self, parent=None, designMode=False):
-        TaurusWidget.__init__(self, parent, designMode=designMode)
-        
-        self._ui = Ui_beamChargeMonitors()
-        self._ui.setupUi(self)
-        
+    def __init__(self, parent=None, name=None, designMode=False):
+        try:
+            self.__name = name.__name__
+        except:
+            self.__name = "beamChargeMonitors"
+        super(beamChargeMonitors, self).__init__(parent, designMode=designMode)
+        try:
+            self.debug("[%s]__init__()" % (self.__name))
+            basePath = os.path.dirname(__file__)
+            if len(basePath) == 0:
+                basePath = '.'
+            self.loadUi(filename="beamChargeMonitors.ui",
+                        path=basePath+"/ui")
+        except Exception as e:
+            self.warning("[%s]__init__(): Widget exception! %s"
+                         % (self.__name, e))
+            traceback.print_exc()
+            self.traceback()
     
     @classmethod
     def getQtDesignerPluginInfo(cls):
