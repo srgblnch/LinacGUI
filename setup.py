srgblnch/LinacGUI

commit cd107d70229e52854bf6fbbfc7d7b5bd1b4233b1

Change shebang to support virtualenv
The generated launcher scripts hardcode the shebang to the python
used at install time. This prevents us from using them on a venv
different than the one they were installed.
Add option in setup.py to change the shebang of the scripts to
"#!/usr/bin/env python".

setup.py
@@ -39,6 +39,11 @@ setup(
             'ctlisetup = ctli.ctlisetup:main',
             ]
         },
+    options={
+        'build_scripts': {
+                'executable': '/usr/bin/env python',
+                    },
+        }
     include_package_data=True,
     package_dir={'widgets': ['widgets/ui']},
     package_data={'widgets': ['*.ui']},

commit 73e466ee86c7b0f9bef7cb7feda06aa4ab1593a2

Fix error introduced in commit cd107d7022
Commit cd107d7022 introduced a syntax error in setup.py due to a
missing comma. Fix it

setup.py
@@ -46,7 +46,7 @@ setup(
         'build_scripts': {
                 'executable': '/usr/bin/env python',
                     },
-        }
+        },
     include_package_data=True,
     package_dir={'widgets': ['widgets/ui']},
     package_data={'widgets': ['*.ui']},