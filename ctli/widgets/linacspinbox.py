7292a5dbf4e22ff09cec9706317b9358249977d4
Sergi Blanch-Torne <sblanch@cells.es>
2018-08-17 11:13:43 +0200

    add the object properties for the danger operation on linac specific spinboxes

--- ctli/widgets/linacspinbox.py
+++ ctli/widgets/linacspinbox.py
@@ -16,7 +16,8 @@
 # ##### END GPL LICENSE BLOCK #####
 
 from taurus.qt.qtgui.input import TaurusValueSpinBox
-from taurus.external.qt import Qt  # QDoubleSpinBox
+from taurus.external.qt import Qt
+from taurus.qt.qtgui.base import TaurusBaseWritableWidget
 
 __author__ = "Sergi Blanch-Torne"
 __copyright__ = "Copyright 2018, CELLS / ALBA Synchrotron"
@@ -32,9 +33,15 @@ __all__ = ["LinacValueSpinBox", "SaveRetrieveSpinBox"]
 
 
 class LinacValueSpinBox(TaurusValueSpinBox):
+
+    _dangerMessage = None
+    _dangerAbove = None
+    _dangerBelow = None
+
     def __init__(self, qt_parent=None, designMode=False):
         TaurusValueSpinBox.__init__(self, qt_parent, designMode)
         self.setEnableWheelEvent(False)
+        self._dangerMessage = ""
 
     def wheelEvent(self, evt):
         """Wheel event handler"""
@@ -54,6 +61,108 @@ class LinacValueSpinBox(TaurusValueSpinBox):
     enableWheelEvent = Qt.pyqtProperty("bool", getEnableWheelEvent,
                                        setEnableWheelEvent,
                                        resetEnableWheelEvent)
+
+    def _reviewIsDangerous(self):
+        if self._dangerAbove is not None or self._dangerBelow is not None:
+            self._isDangerous = len(self._dangerMessage) > 0
+            self.info("Dangerous operation feature active")
+        else:
+            self._isDangerous = False
+            self.info("Dangerous operation feature INactive")
+
+    def setDangerMessage(self, dangerMessage=""):
+        """Sets the danger message when applying an operation.
+           In this widget, setting a danger message is not enough to define
+           a dangerous operation, because it must be defined the thresholds
+           where the danger operation applies.
+           If dangerMessage is None, the apply operation is considered safe
+
+        :param dangerMessage: (str or None) the danger message.
+                              If None is given (default) the apply operation
+                              is considered safe.
+        """
+        self._dangerMessage = dangerMessage
+        self.info("setting a danger message %r" % (self._dangerMessage))
+        self._reviewIsDangerous()
+
+    DangerMessage = Qt.pyqtProperty("QString",
+                                    TaurusBaseWritableWidget.getDangerMessage,
+                                    setDangerMessage,
+                                    TaurusBaseWritableWidget.
+                                    resetDangerMessage)
+
+    def getDangerAbove(self):
+        return self._dangerAbove or float('Inf')
+
+    def setDangerAbove(self, value):
+        self._dangerAbove = value
+        self.info("setting dangerous setpoint above %g" % value)
+        self._reviewIsDangerous()
+
+    def resetDangerAbove(self):
+        self.setDangerAbove(None)
+
+    DangerAbove = Qt.pyqtProperty('int', getDangerAbove, setDangerAbove,
+                                  resetDangerAbove)
+
+    def getDangerBelow(self):
+        return self._dangerBelow or float('-Inf')
+
+    def setDangerBelow(self, value):
+        self._dangerBelow = value
+        self.info("setting dangerous setpoint below %g" % value)
+        self._reviewIsDangerous()
+
+    def resetDangerBelow(self):
+        self.setDangerBelow(None)
+
+    DangerBelow = Qt.pyqtProperty('int', getDangerBelow, setDangerBelow,
+                                  resetDangerBelow)
+
+    def isDangerAbove(self):
+        return self._dangerAbove is not None and \
+               self.getValue() > self._dangerAbove
+
+    def isDangerBelow(self):
+        return self._dangerBelow is not None and \
+               self.getValue() < self._dangerBelow
+
+    def isDangerousAction(self):
+        return self.isDangerAbove() or self.isDangerBelow()
+
+    def safeApplyOperations(self, ops=None):
+        '''Modify the behaviour of the DangerMessage because in this case,
+           different than a button, we may distinguish between set action and
+           unset action.
+           It seems that Taurus will someday support multiple Danger messages,
+           but this widget did not copy this feature.
+        '''
+        if ops is None:
+            ops = self.getPendingOperations()
+        self.info("safeApplyOperations(ops=%s)" % (ops))
+        # Check if we need to take care of dangerous operations
+        if self.getForceDangerousOperations():
+            dangerMsgs = []
+        else:
+            dangerMsgs = [op.getDangerMessage() for op in ops
+                          if len(op.getDangerMessage()) > 0]
+        # warn the user if need be
+        if self.isDangerousAction():
+            WarnTitle = "Potentially dangerous action"
+            WarnMsg = "%s\nProceed?" % (dangerMsgs[0])
+            Buttons = Qt.QMessageBox.Ok | Qt.QMessageBox.Cancel
+            Default = Qt.QMessageBox.Cancel
+            result = Qt.QMessageBox.warning(self, WarnTitle, WarnMsg,
+                                            Buttons, Default)
+            # Important default behaviour shall be the Cancel ---
+            if result == Qt.QMessageBox.Cancel:
+                self.info("safeApplyOperations(...) answer = Cancel")
+                self._OperationCancelled()
+            else:
+                self.info("safeApplyOperations(...) answer = Ok")
+                self.applyPendingOperations(ops)
+        else:
+            self.applyPendingOperations(ops)
 
 
 class SaveRetrieveSpinBox(Qt.QSpinBox):
