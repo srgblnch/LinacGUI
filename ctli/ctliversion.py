79d8f35a73ae0ac8bfb26a6271eb754bb8d29778
Sergi Blanch-Torne <sblanch@cells.es>
2018-08-20 09:34:26 +0200

    Bump version 2.73.6 to 2.73.7-alpha

--- ctli/ctliversion.py
+++ ctli/ctliversion.py
@@ -24,7 +24,7 @@ __license__ = "GPLv3+"
 
 # we use semantic versioning (http://semver.org/) and we update it using the
 # bumpversion script (https://github.com/peritus/bumpversion)
-__version = '__version = '2.73.6''
+__version = '__version = '2.73.7-alpha''
 
 
 def version():
