10c61564505ce54f7d0e5f0e65136a3518dac1e6
Sergi Blanch-Torne <sblanch@cells.es>
2019-04-23 16:39:40 +0200

    Bump version 2.73.8-alpha to 2.73.8

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ __license__ = "GPLv3+"
 
 # The version is updated automatically with bumpversion
 # Do not update manually
-__version = '2.73.8-alpha'  
+__version = '2.73.8'  
 
 
 from setuptools import setup, find_packages
